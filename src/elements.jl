450e73811f2dd881159a76426bbb68ea3a15687d
Yuri <yuri.shimane@gmail.com>
2025-12-15 14:59:20 -0800

    elements implemented

--- src/elements.jl
+++ src/elements.jl
@@ -41,24 +41,49 @@ function kep2rv(kep::Array{<:Real,1}, μ::Real)
 end
 
 
-function kep2meesma(kep::Array{<:Real,1})
+function rv2kep(rv::Array{<:Real,1}, μ::Real)
+    rvec = rv[1:3]
+    vvec = rv[4:6]
+    hvec = cross(rvec, vvec)
+    r, v, h = norm(rvec), norm(vvec), norm(hvec)
+    evec = (1/μ) * cross(vvec, hvec) - rvec / r
+    e = norm(evec)
+    i = acos(hvec[3] / h)
+    if 0 < i < π
+        ndir = cross([0,0,1], hvec/h)
+    else
+        ndir = [1,0,0]
+    end
+    Ω = atan(ndir[2], ndir[1])
+    a = (r * μ) / (2*μ - r*v^2)
+    θ = atan(h*dot(rvec, vvec)/r, h^2/r - μ)
+    px = dot(rvec, ndir)
+    py = dot(rvec, cross(hvec, ndir))/h
+    ω = atan(py, px) - θ
+    return [a,e,i,Ω,ω,θ]
+end
+
+
+function kep2mee(kep::Array{<:Real,1})
     # unpack
     a, e, i, Ω, ω, θ = kep
     # compute MEEs
+    p = a * (1 - e^2)
     f = e*cos(Ω+ω)
     g = e*sin(Ω+ω)
     h = tan(i/2)*cos(Ω)
     k = tan(i/2)*sin(Ω)
     l = Ω + ω + θ
-    return [a,f,g,h,k,l]
+    return [p,f,g,h,k,l]
 end
 
 
-function meesma2kep(meesma::Array{<:Real,1})
+function mee2kep(mee::Array{<:Real,1})
     # unpack
-    a, f, g, h, k, l = meesma
+    p, f, g, h, k, l = mee
     # compute Keplerian elements
     e = sqrt(f^2 + g^2)
+    a = p / (1 - e^2)
     i = atan(2*sqrt(h^2+k^2), 1-h^2-k^2)
     Ω = atan(k,h)
     ω = atan(g*h-f*k, f*h+g*k)
@@ -67,6 +92,72 @@ function meesma2kep(meesma::Array{<:Real,1})
 end
 
 
-function meesma2rv(meesma::Array{<:Real,1}, μ::Real)
-    return kep2rv(meesma2kep(meesma), μ)
+function mee2rv(mee::Array{<:Real,1}, μ::Real)
+    p, f, g, h, k, l = mee
+    sinL, cosL = sin(l), cos(l)
+    α2 = h^2 - k^2
+    s2 = 1 + h^2 + k^2
+    w = 1 + f * cosL + g * sinL
+    r = p / w
+    sqrt_mup = sqrt(μ / p)
+
+    r = [r/s2 * (cosL + α2 * cosL + 2 * h * k * sinL);
+         r/s2 * (sinL - α2 * sinL + 2 * h * k * cosL);
+         2r/s2 * (h * sinL - k * cosL)]
+    v = [-1/s2 * sqrt_mup * ( sinL + α2 * sinL - 2 * h * k * cosL + g - 2*f*h*k + α2 * g);
+         -1/s2 * sqrt_mup * (-cosL + α2 * cosL + 2 * h * k * sinL - f + 2*g*h*k + α2 * f);
+         2/s2 * sqrt_mup * (h * cosL + k * sinL + f * h + g * k)]
+    return [r; v]
+end
+
+
+function rv2mee(rv::Array{<:Real,1}, μ::Real, retrograde::Bool = false)
+    rvec = rv[1:3]
+    vvec = rv[4:6]
+    hvec = cross(rvec, vvec)
+    r, v, h = norm(rvec), norm(vvec), norm(hvec)
+    w = hvec / h
+    evec = (1/μ) * cross(vvec, hvec) - rvec / r
+
+    # compute h, k
+    h,k = hvec[1:2]
+
+    # compute equinoctial frame
+    is_retro = retrograde ? -1 : 1
+    k =  w[1] / (1 + is_retro * w[3]);
+    h = -w[2] / (1 + is_retro * w[3]);
+    den = k * k + h * h + 1;
+    fv = [(1. - k * k + h * h) / den;
+          (2. * k * h) / den;
+          (-2. * is_retro * k) / den]
+
+    gv = [(2. * is_retro * k * h) / den;
+          (1. + k * k - h * h) * is_retro / den;
+          (2. * h) / den]
+
+    # compute f, g
+    g = dot(evec, gv)
+    f = dot(evec, fv)
+
+    # compute L
+    det1 = (gv[2]*fv[1]-fv[2]*gv[1])    # xy
+    det2 = (gv[3]*fv[1]-fv[3]*gv[1])    # xz
+    det3 = (gv[3]*fv[2]-fv[3]*gv[2])    # yz
+    max_det = max(abs(det1), abs(det2), abs(det3));
+
+    if abs(det1) == max_det
+        X = ( gv[2]*rvec[1] - gv[1] * rvec[2]) / det1
+        Y = (-fv[2]*rvec[1] + fv[1] * rvec[2]) / det1
+    elseif abs(det2) == max_det
+        X = ( gv[3]*rvec[1] - gv[1] * rvec[3]) / det2
+        Y = (-fv[3]*rvec[1] + fv[1] * rvec[3]) / det2
+    else
+        X = ( gv[3]*rvec[2] - gv[2] * rvec[3]) / det3
+        Y = (-fv[3]*rvec[2] + fv[2] * rvec[3]) / det3
+    end
+    L = atan(Y/r, X/r)
+
+    # compute p
+    p = r * (1 + f * cos(L) + g * sin(L))
+    return [p,f,g,h,k,L]
 end
--- test/test_elements.jl
+++ test/test_elements.jl
@@ -4,7 +4,7 @@ using Test
 
 include(joinpath(@__DIR__, "..", "src", "AstrodynamicsCore.jl"))
 
-@testset "Elements" begin
+@testset "kep2rv" begin
     # from https://naif.jpl.nasa.gov/pub/naif/PSYCHE/kernels/spk/de440.bsp.lbl
     # test case 1
     MU = 398600.435507
@@ -15,6 +15,15 @@ include(joinpath(@__DIR__, "..", "src", "AstrodynamicsCore.jl"))
     RV = AstrodynamicsCore.kep2rv(COE, MU)
     @test RV ≈ RV_CHECK rtol=1e-8
 
+    COE_BACK = AstrodynamicsCore.rv2kep(RV, MU)
+    ΔCOE = [COE_BACK[1] - COE[1]; 
+            COE_BACK[2] - COE[2]; 
+            acos(cos(COE_BACK[3] - COE[3]));
+            acos(cos(COE_BACK[4] - COE[4]));
+            acos(cos(COE_BACK[5] - COE[5]));
+            acos(cos(COE_BACK[6] - COE[6]))];
+    @test ΔCOE ≈ zeros(6) atol=1e-10
+
     # test case 2
     COE = [26590, 0.06, deg2rad(15), deg2rad(109), deg2rad(27), deg2rad(58)];
     R_CHECK = [-24090.96849278776, -5928.260586177573, 6620.625587995996];
@@ -22,4 +31,54 @@ include(joinpath(@__DIR__, "..", "src", "AstrodynamicsCore.jl"))
     RV_CHECK = [R_CHECK; V_CHECK]
     RV = AstrodynamicsCore.kep2rv(COE, MU)
     @test RV ≈ RV_CHECK rtol=1e-8
+
+    COE_BACK = AstrodynamicsCore.rv2kep(RV, MU)
+    ΔCOE = [COE_BACK[1] - COE[1]; 
+            COE_BACK[2] - COE[2]; 
+            acos(cos(COE_BACK[3] - COE[3]));
+            acos(cos(COE_BACK[4] - COE[4]));
+            acos(cos(COE_BACK[5] - COE[5]));
+            acos(cos(COE_BACK[6] - COE[6]))];
+    @test ΔCOE ≈ zeros(6) atol=1e-10
+end
+
+@testset "mee2rv" begin
+    # test case 1
+    MU = 398600.435507
+    COE = [8000, 0.12, deg2rad(85), deg2rad(240), deg2rad(200), deg2rad(130)]
+    R_CHECK = [-4022.024182950954, -6221.706935396668, -4255.655925778433]
+    V_CHECK = [-1.519176635355084, -3.564858730446071, 5.33536331860247]
+    RV_CHECK = [R_CHECK; V_CHECK]
+
+    MEE = AstrodynamicsCore.kep2mee(COE)
+    RV = AstrodynamicsCore.mee2rv(MEE, MU)
+    @test RV ≈ RV_CHECK rtol=1e-8
+
+    MEE_BACK = AstrodynamicsCore.rv2mee(RV, MU)
+    ΔMEE = [MEE_BACK[1] - MEE[1];
+            MEE_BACK[2] - MEE[2];
+            MEE_BACK[3] - MEE[3];
+            MEE_BACK[4] - MEE[4];
+            MEE_BACK[5] - MEE[5];
+            acos(cos(MEE_BACK[6] - MEE[6]))];
+    @test ΔMEE ≈ zeros(6) atol=1e-10
+
+    # test case 2
+    COE = [26590, 0.06, deg2rad(15), deg2rad(109), deg2rad(27), deg2rad(58)];
+    R_CHECK = [-24090.96849278776, -5928.260586177573, 6620.625587995996];
+    V_CHECK = [0.7942641884144943, -3.924902041686212, 0.1411640937441778];
+    RV_CHECK = [R_CHECK; V_CHECK]
+
+    MEE = AstrodynamicsCore.kep2mee(COE)
+    RV = AstrodynamicsCore.mee2rv(MEE, MU)
+    @test RV ≈ RV_CHECK rtol=1e-8
+
+    MEE_BACK = AstrodynamicsCore.rv2mee(RV, MU)
+    ΔMEE = [MEE_BACK[1] - MEE[1];
+            MEE_BACK[2] - MEE[2];
+            MEE_BACK[3] - MEE[3];
+            MEE_BACK[4] - MEE[4];
+            MEE_BACK[5] - MEE[5];
+            acos(cos(MEE_BACK[6] - MEE[6]))];
+    @test ΔMEE ≈ zeros(6) atol=1e-10
 end
